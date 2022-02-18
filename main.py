3d894eacdfd7b51be1a7d3c6f0dd934c18d295dc
Tal Revah <tal.revah@gmail.com>
2022-02-18 11:47:19 +0200

    changing main message

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   response = {"message":"Hello World!"}
+   response = {"message":"Tal say hye!"}
    return jsonify(response)
 
 if __name__ == "__main__":
